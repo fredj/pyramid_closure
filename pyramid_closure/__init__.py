2c7bdba5a14bbd9e706e230a64c96919fb76ca3e
Éric Lemoine <eric.lemoine@gmail.com>
2014-06-09 18:55:08 +0200

    Add a static view

--- pyramid_closure/__init__.py
+++ pyramid_closure/__init__.py
@@ -9,5 +9,6 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+    config.add_static_view('static', 'static', cache_max_age=3600)
     config.scan()
     return config.make_wsgi_app()
