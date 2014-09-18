178490895a4deb174a156d78e071c4b276171d0d
Éric Lemoine <eric.lemoine@gmail.com>
2014-09-18 14:01:45 +0200

    Add static views for closure, ol and ngeo

--- pyramid_closure/__init__.py
+++ pyramid_closure/__init__.py
@@ -8,7 +8,17 @@ def includeme(config):
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    closure_library_path = settings.get(
+        'pyramid_closure.closure_library_path')
+    openlayers_library_path = settings.get(
+        'pyramid_closure.openlayers_library_path')
+    ngeo_library_path = settings.get(
+        'pyramid_closure.ngeo_library_path')
+
     config = Configurator(settings=settings)
+    config.add_static_view('closure', closure_library_path, cache_max_age=3600)
+    config.add_static_view('ol', openlayers_library_path, cache_max_age=3600)
+    config.add_static_view('ngeo', ngeo_library_path, cache_max_age=3600)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.scan()
     return config.make_wsgi_app()
