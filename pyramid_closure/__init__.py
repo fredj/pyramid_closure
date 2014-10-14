0341cc4dbaeebbe50e8593484150e68d631eacc8
Éric Lemoine <eric.lemoine@gmail.com>
2014-10-14 15:27:38 +0200

    pyramid_closure no longer creates static views

--- pyramid_closure/__init__.py
+++ pyramid_closure/__init__.py
@@ -11,14 +11,3 @@ def includeme(config):
     config.add_view('pyramid_closure.views:depsjs', route_name='deps.js',
                     renderer='string')
     config.add_route('deps.js', '/closure-deps.js', request_method='GET')
-
-    # get file system paths to closure, openlayers and ngeo from settings
-    settings = config.get_settings()
-    closure_path = settings.get('pyramid_closure.closure_path')
-    openlayers_path = settings.get('pyramid_closure.openlayers_path')
-    ngeo_path = settings.get('pyramid_closure.ngeo_path')
-
-    # add static views for closure, openlayers and ngeo
-    config.add_static_view('closure', closure_path, cache_max_age=3600)
-    config.add_static_view('ol', openlayers_path, cache_max_age=3600)
-    config.add_static_view('ngeo', ngeo_path, cache_max_age=3600)
