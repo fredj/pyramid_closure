a3ef440893009aadbbef98731b5752ce87c62409
Éric Lemoine <eric.lemoine@gmail.com>
2015-02-26 10:22:55 +0100

    Use static_url in deps view

--- pyramid_closure/views.py
+++ pyramid_closure/views.py
@@ -1,14 +1,17 @@
-from itertools import izip
+from itertools import izip, chain
 
 from pyramid.settings import aslist
 
 from .closure import depswriter
 
 
+def flatten(l):
+    "Flatten one level of nesting"
+    return chain.from_iterable(l)
+
+
 def pairwise(iterable):
-    if isinstance(iterable, dict):
-        return iterable.iteritems()
-    a = iter(aslist(iterable))
+    a = flatten(iterable)
     return izip(a, a)
 
 
@@ -20,17 +23,28 @@ def depsjs(request):
 
     roots = pyramid_closure.get("roots") if pyramid_closure else \
         settings.get("pyramid_closure.roots")
-    roots = aslist(roots or [])
+
+    if roots is None:
+        roots = []
+    elif isinstance(roots, basestring):
+        roots = aslist(roots)
+
     for root in roots:
         path_to_source.update(depswriter._GetRelativePathToSourceDict(root))
 
     roots_with_prefix = pyramid_closure.get("roots_with_prefix") if \
         pyramid_closure else \
         settings.get("pyramid_closure.roots_with_prefix")
-    roots_with_prefix = pairwise(roots_with_prefix or {})
-    for prefix, root in roots_with_prefix:
+
+    if roots_with_prefix is None:
+        roots_with_prefix = []
+    elif isinstance(roots_with_prefix, basestring):
+        roots_with_prefix = aslist(roots_with_prefix)
+
+    for prefix, root in pairwise(roots_with_prefix):
         path_to_source.update(
-            depswriter._GetRelativePathToSourceDict(root, prefix=prefix))
+            depswriter._GetRelativePathToSourceDict(
+                root, prefix=request.static_url(prefix)))
 
     request.response.content_type = 'text/javascript'
 
